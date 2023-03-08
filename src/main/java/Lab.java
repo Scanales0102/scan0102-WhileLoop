850a2327c26653b5ef3bd3e77cecc74f1c2a2d0b
Sebastian Canales <spideronyou100@gmail.com>
2023-03-08 07:14:08 +0000

    complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -35,9 +35,14 @@ public class Lab {
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
-
-//        code here
-
+    
+        while (i < end) {
+            sum += i;
+            i++;
+        }
+    
         return sum;
     }
-}
+    
+    }
+
